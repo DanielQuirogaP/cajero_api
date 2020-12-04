31a8e275dcf1c55bc934ec10b78b87c62727f5cc
Daniel Quiroga <danielquirogaparra@gmail.com>
2020-12-04 11:47:56 -0500

    Path bug was fixed

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from cajero_api.db.user_db import UserInDB
-from cajero_api.db.user_db import update_user, get_user
-from cajero_api.db.transaction_db import TransactionInDB
-from cajero_api.db.transaction_db import save_transaction
-from cajero_api.models.user_models import UserIn, UserOut
-from cajero_api.models.transaction_models import TransactionIn, TransactionOut
+from db.user_db import UserInDB
+from db.user_db import update_user, get_user
+from db.transaction_db import TransactionInDB
+from db.transaction_db import save_transaction
+from models.user_models import UserIn, UserOut
+from models.transaction_models import TransactionIn, TransactionOut
 
 import datetime
 from fastapi import FastAPI
